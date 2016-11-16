0dbd8f0b9fbfdfd43afc64c4a21582ed118e7be4
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2016-11-16 22:33:18 +0300

    Updated packages, more research

--- research/data-preparation.fsx
+++ research/data-preparation.fsx
@@ -1,4 +1,4 @@
-#load "./../packages/FsLab/FsLab.fsx"
+#load "packages/FsLab/FsLab.fsx"
 
 open System
 open System.IO
@@ -6,33 +6,84 @@ open System.Collections.Generic
 
 open FsLab
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let FormatLogFileToJsonArrayFile (inputPath: string) (outputPath: string) =
-    use reader = new StreamReader(inputPath)
+let ConvertLogsToJsonArray (inputPaths: seq<string>) (outputPath: string) =
     use writer = new StreamWriter(outputPath)
     writer.WriteLine("[")
-    let mutable i = 0;
-    while (not reader.EndOfStream) do
-        let isFirstEntry = i = 0
-        let line = "  " + reader.ReadLine().TrimEnd(',')
-        let isLastEntry = reader.EndOfStream
-        match (isFirstEntry, isLastEntry) with
-            | true, false ->
-                writer.Write(line)
-            | (_, false) ->
-                writer.WriteLine(",")
-                writer.Write(line)
-            | (_, true) ->
-                try
-                    JsonValue.Parse(line) |> ignore
-                    writer.Write(",")
-                    writer.WriteLine(line)
-                with _ ->
-                    writer.WriteLine()
-                    printfn "Input file is broken."
-        i <- i + 1
+    let mutable recordIndex = 0
+    let iterate index (inputPath: string) =
+        use reader = new StreamReader(inputPath)
+        while (not reader.EndOfStream) do
+            let isFirstEntry = recordIndex = 0
+            let line = "  " + reader.ReadLine().TrimEnd(',')
+            let isLastEntry = reader.EndOfStream && index = Seq.length inputPaths - 1
+            match (isFirstEntry, isLastEntry) with
+                | true, false ->
+                    writer.Write(line)
+                | (_, false) ->
+                    writer.WriteLine(",")
+                    writer.Write(line)
+                | (_, true) ->
+                    try
+                        JsonValue.Parse(line) |> ignore
+                        writer.Write(",")
+                        writer.WriteLine(line)
+                    with _ ->
+                        writer.WriteLine()
+                        printfn "Input file %s is broken." inputPath
+            recordIndex <- recordIndex + 1
+    inputPaths |> Seq.iteri iterate
     writer.WriteLine("]")
 
-FormatLogFileToJsonArrayFile "bot-sample-raw.log.json" "bot-sample.log.json"
+let IsDuration (log: JsonValue) =
+    let props = log?Properties
+    match props with
+        | JsonValue.Record(props) when props |> Seq.exists (fun (k, v) -> k = "TimedOperationElapsedInMs")
+            -> true
+        | _ -> false
+
+let ExtractDuration (log: JsonValue)  =
+    let props = log?Properties
+    seq {
+        yield log?Timestamp.AsDateTime().Ticks :> Object
+        yield props?InstanceId.AsString() :> Object
+        yield props?TimedOperationDescription.AsString() :> Object
+        yield props?TimedOperationElapsedInMs.AsInteger() :> Object
+    }
+
+let ExtractRows (headers: seq<string>) 
+                filter 
+                (mapper: JsonValue -> seq<obj>) 
+                (inputPaths: seq<string>) 
+                (outputPath: string) =
+    use writer = new StreamWriter(outputPath)
+    writer.WriteLine(String.Join(",", headers))
+    let extract (inputPath: string) =
+        use reader = new StreamReader(inputPath)
+        while (not reader.EndOfStream) do
+            let json = reader.ReadLine().TrimEnd(',')
+            try
+                let log = JsonValue.Parse(json)
+                if filter log then
+                    let row = mapper log
+                    writer.WriteLine(String.Join(",", row))
+            with e ->
+                printfn "Skipped invalid json line: %O" e
+    inputPaths |> Seq.iter extract
+
+let ExtractDurations (inputPaths: seq<string>) (outputPath: string)= 
+    ExtractRows ["Timestamp"; "InstanceId"; "Name"; "Ms"] IsDuration ExtractDuration inputPaths outputPath
+
+let T4x16x16 = Directory.GetFiles("logs/loadtest-16-11-14__06-49-57")
+
+ExtractDurations T4x16x16 "samples/4x16x16-durations.csv"
+
+
+//FormatLogFileToJsonArrayFile "bot-sample-raw.log.json" "bot-sample.log.json"
+
+ConvertLogsToJsonArray
+    [@"logs\loadtest-16-11-14__06-49-57\loadtest__16-11-14__06-49-57__3-log.json"]
+    "samples/4x16x16-bot.log.json"
--- research/notebook.fsx
+++ research/notebook.fsx
@@ -7,51 +7,115 @@ open System.Collections.Generic
 
 open FsLab
 open FSharp.Data
-open XPlot.GoogleCharts
+//open XPlot.GoogleCharts
+open FSharp.Charting
 open Deedle
 open MathNet.Numerics.Statistics
-
-type Log = JsonProvider<"D:/Personal/Conreign/research/bot-sample.log.json">
 
 let percentile p (s: Series<'a, float>) =
     s.Values.Percentile(p)
 
-let IsDuration (log: Log.Root) = log.Properties.TimedOperationElapsedInMs.IsSome
-let IsCounter (log: Log.Root) = log.Properties.CounterName.IsSome
+//type Log = JsonProvider<"D:/Personal/Conreign/research/bot-sample.log.json">
+type DurationLog = CsvProvider<"D:/Personal/Conreign/research/samples/sample-durations.csv">
+let FullPath path = Path.Combine(__SOURCE_DIRECTORY__, path)
 
-let MapDurationName (log: Log.Root) = log.Properties.TimedOperationDescription.Value
+let durationLog = DurationLog.Load(FullPath "samples/4x16x16-durations.csv")
 
-let ExtractDurationSeries (items: seq<Log.Root>)  =
-    items |> Seq.filter IsDuration
-          |> Seq.mapi (fun i x -> i, x.Properties.TimedOperationElapsedInMs.Value)
-          |> series
+let histograms =
+    durationLog.Rows
+    |> Seq.map (fun x -> x.Name, x.Ms)
+    |> Seq.groupBy fst
+    //|> Seq.map (fun (k, vs) ->  vs |> Chart.Histogram |> Chart.WithTitle k)
+    |> Seq.toList
 
-let ExtractCounterSeries (items: seq<Log.Root>) =
-    items |> Seq.filter IsCounter
-          |> Seq.distinctBy (fun i -> i.Timestamp)
-          |> Seq.map (fun i -> i.Timestamp, i.Properties.CounterValue.Value)
-          |> series
+histograms.[0]
+histograms.[1]
+histograms.[2]
 
-let logs = Log.Load(@"D:\Personal\Conreign\research\samples\4x16x16-bot.log.json")
+let createDurationSeries (agg: seq<float> -> float) (logs: seq<DurationLog.Row>)  =
+    logs
+    |> Seq.map (fun x -> x.Timestamp, x.Ms)
+    |> Seq.groupBy fst
+    |> Seq.map (fun (t, vs) -> new DateTime(t), vs |> Seq.map (snd >> float) |> agg)
+    |> Seq.sortBy fst
+    |> Series.ofObservations
+
+let durationSeries =
+    durationLog.Rows
+    |> Seq.groupBy (fun x -> x.Name)
+    |> Series.ofObservations
+    |> Series.mapValues (createDurationSeries (fun p -> p.Percentile(95)))
+
+let signalR = durationSeries |> Series.get "SignalR.Send";
+
+signalR
+|> Series.observations
+|> Chart.FastLine
+
+signalR
+|> Series.windowDistInto (TimeSpan(0, 0, 10)) (percentile 90)
+|> Series.observations
+|> Chart.FastLine
+
+
+
+//percentiles
+//percentiles |> Frame.ofColumns |> Chart.Line
 
 let durations =
-    logs |> Seq.filter IsDuration
-         |> Seq.groupBy MapDurationName
-         |> Seq.map (fun (name, values) -> (name, ExtractDurationSeries values))
-         |> Frame.ofColumns
+    Frame.ReadCsv (FullPath "samples/4x16x16-durations.csv")
+    |> Frame.sortRows "Timestamp"
 
-let durationNames = durations.ColumnKeys |> Seq.toList
+durations
 
-let signalRSeries =
-    durations.GetColumn<float>("SignalR.Send")
-        |> Series.dropMissing
-        |> Series.windowInto 200 (percentile 95)
 
-let botHandleSeries =
-    durations.GetColumn<float>("Bot.Handle")
-        |> Series.dropMissing
-        |> Series.windowInto 100 (percentile 95)
 
-botHandleSeries |> Chart.Line |> Chart.WithSize (1600, 900)
-
-    
+//let percentile p (s: Series<'a, float>) =
+//    s.Values.Percentile(p)
+//
+//let IsDuration (log: Log.Root) = log.Properties.TimedOperationElapsedInMs.IsSome
+//let IsCounter (log: Log.Root) = log.Properties.CounterName.IsSome
+//
+//let MapDurationName (log: Log.Root) = log.Properties.TimedOperationDescription.Value
+//let MapDurationValue (log: Log.Root) = log.Properties.TimedOperationElapsedInMs.Value
+//
+//let ExtractDurationSeries (items: seq<Log.Root>)  =
+//    items |> Seq.filter IsDuration
+//          |> Seq.groupBy (fun l -> l.Timestamp)
+//          |> Seq.map (fun (t, values) -> t, values |> Seq.map MapDurationValue |> Seq.map float |> (fun s -> s.Percentile(95)))
+//          |> Seq.sortBy fst
+//          |> series
+//
+//let ExtractCounterSeries (items: seq<Log.Root>) =
+//    items |> Seq.filter IsCounter
+//          |> Seq.distinctBy (fun i -> i.Timestamp)
+//          |> Seq.map (fun i -> i.Timestamp, i.Properties.CounterValue.Value)
+//          |> series
+//
+//let logs = Log.Load(@"D:\Personal\Conreign\research\samples\4x16x16-bot.log.json")
+//
+//let durations =
+//    logs |> Seq.filter IsDuration
+//         |> Seq.groupBy MapDurationName
+//         |> Seq.map (fun (name, values) -> (name, ExtractDurationSeries values))
+//         |> Frame.ofColumns
+//
+//let durationNames = durations.ColumnKeys |> Seq.toList
+//
+//let signalRSeries =
+//    durations.GetColumn<float>("SignalR.Send")
+//        |> Series.dropMissing
+//        |> Series.windowInto 200 (percentile 95)
+//
+//let botHandleSeries =
+//    durations.GetColumn<float>("Bot.Handle")
+//        |> Series.dropMissing
+//        |> Series.windowInto 1000 (percentile 95)
+//
+//botHandleSeries |> Chart.Line
+//
+//logs |> Seq.filter IsDuration
+//     |> Seq.mapi (fun i l -> (i.ToString()), MapDurationValue l)
+//     |> Seq.filter (fun (k, v) -> v < 1800)
+//     |> Series.ofObservations
+//     |> Chart.Histogram
