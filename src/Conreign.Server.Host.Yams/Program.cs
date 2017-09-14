4e8126828b4534b800a77b5973ffd2a18a74d4bf
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2017-09-14 23:37:04 +0300

    Added YAMS host project

---
+++ src/Conreign.Server.Host.Yams/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Security.Permissions;
+using Etg.Yams;
+using Topshelf;
+
+namespace Conreign.Server.Host.Yams
+{
+    internal class Program
+    {
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+        private static void Main(string[] args)
+        {
+            ServicePointManager.DefaultConnectionLimit = 10000;
+            HostFactory.Run(host =>
+            {
+                var configurationDirectory = Environment.CurrentDirectory;
+                var environment = YamsServiceConfiguration.DefaultEnvironment;
+
+                host.AddCommandLineDefinition("config", x => configurationDirectory = x);
+                host.AddCommandLineDefinition("env", x => environment = x);
+                host.ApplyCommandLine();
+                var configuration = YamsServiceConfiguration.Load(configurationDirectory, environment);
+
+                host.Service<IYamsService>(service =>
+                {
+                    service.ConstructUsing(name =>
+                    {
+                        var builder = new YamsConfigBuilder(
+                            configuration.ClusterId,
+                            configuration.InstanceUpdateDomain,
+                            configuration.InstanceUpdateDomain,
+                            configuration.ApplicationInstallDirectory);
+                        var yamsConfig = builder
+                            .SetCheckForUpdatesPeriodInSeconds(configuration.UpdatePeriodInSeconds)
+                            .SetApplicationRestartCount(configuration.RestartCount)
+                            .Build();
+                        return YamsServiceFactory.Create(
+                            yamsConfig, 
+                            configuration.AzureStorageConnectionString, 
+                            configuration.AzureStorageConnectionString);
+                    });
+                    service.WhenStarted(x => x.Start().Wait());
+                    service.WhenStopped(x => x.Stop().Wait());
+                });
+
+                host.RunAsLocalSystem();
+                host.SetDescription("Yams Service Host");
+                host.SetDisplayName("Yams Service");
+                host.SetServiceName("Yams");
+                host.StartAutomatically();
+            });
+        }
+    }
+}
---
+++ src/Conreign.Server.Host.Yams/YamsServiceConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Conreign.Server.Host.Yams
+{
+    public class YamsServiceConfiguration
+    {
+        public const string DefaultEnvironment = "local";
+
+        public string Environment { get; }
+        public string ClusterId { get; set; }
+        public string InstanceId { get; set; }
+        public string InstanceUpdateDomain { get; set; }
+        public string ApplicationInstallDirectory { get; set; }
+        public int UpdatePeriodInSeconds { get; set; }
+        public int RestartCount { get; set; }
+        public string AzureStorageConnectionString { get; set; }
+
+        private YamsServiceConfiguration(string environment = DefaultEnvironment)
+        {
+            Environment = environment;
+            InstanceId = System.Environment.MachineName;
+            ApplicationInstallDirectory = Path.Combine(System.Environment.CurrentDirectory, "Applications");
+            UpdatePeriodInSeconds = 15;
+            RestartCount = 3;
+            AzureStorageConnectionString = "UseDevelopmentStorage=true";
+        }
+
+        public static YamsServiceConfiguration Load(string baseDirectory = null, string environment = DefaultEnvironment)
+        {
+            baseDirectory = string.IsNullOrEmpty(baseDirectory) ? System.Environment.CurrentDirectory : baseDirectory;
+            var builder = new ConfigurationBuilder();
+            builder
+                .SetBasePath(baseDirectory)
+                .AddJsonFile("yams.config.json", false)
+                .AddJsonFile("yams.secrets.json", true)
+                .AddJsonFile($"yams.{environment}.config.json", true)
+                .AddJsonFile($"yams.{environment}.secrets.json", true);
+            var config = new YamsServiceConfiguration(environment);
+            var configRoot = builder.Build();
+            configRoot.Bind(config);
+            return config;
+        }
+    }
+}
